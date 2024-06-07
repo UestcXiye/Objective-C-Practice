64274c71eb578206d0af5cc8039ff962d60b7cf5
UestcXiye <812288728@qq.com>
2024-06-07 18:00:50 +0800

    create Constants

---
+++ Constants/Constants/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  Constants
+//
+//  Created by 刘文晨 on 2024/6/7.
+//
+
+#import <Foundation/Foundation.h>
+
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"\u03c0 is %f", M_PI);
+        NSLog(@"%d is larger", MAX(10, 12));
+        
+        NSLocale *here = [NSLocale currentLocale];
+        NSString *currency = [here objectForKey:NSLocaleCurrencyCode];
+        NSLog(@"Money is %@", currency);
+    }
+    return 0;
+}
